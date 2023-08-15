f0f388125eab773cca773dfe5059c846f17a430f
bob-von-bob <stanko.zecevic@example.com>
2023-08-15 22:25:38 +0200

    Update shellcheck_hook
    Shellcheck should check all commited *.sh files before exiting.

--- shellcheck_hook.sh
+++ shellcheck_hook.sh
@@ -13,28 +13,31 @@ fi
 # Initialize an empty array to hold shell script filenames
 SH_FILES=()
 
-# Iterate through staged files
+# Iterate through staged files and collect .sh filenames
 while IFS= read -r file; do
     if [[ "$file" == *.sh ]]; then
         SH_FILES+=("$file")
     fi
 done < <(git diff --cached --name-only --diff-filter=ACMRT)
 
-# Check if there are any .sh files to process
-if [[ ${#SH_FILES[@]} -eq 0 ]]; then
-    echo "No shell script files found for checking. Pre-commit hook completed successfully."
-    exit 0
-fi
+# Initialize the SHELLCHECK_ERROR counter
+SHELLCHECK_ERROR=0
 
 # Iterate through the array of shell script filenames
 for file in "${SH_FILES[@]}"; do
     echo "Checking $file with shellcheck..."
     
-    if  ! shellcheck "$file"; then
-        echo "Shellcheck found issues in $file. Commit aborted."
-        exit 1
+    if ! shellcheck "$file"; then
+        echo "Shellcheck found issues in $file."
+        ((SHELLCHECK_ERROR++))
     fi
 done
+
+# Check if any errors were found
+if [[ $SHELLCHECK_ERROR -ne 0 ]]; then
+    echo "Commit aborted due to shellcheck errors."
+    exit 1
+fi
 
 echo "Pre-commit hook completed successfully."
 exit 0
